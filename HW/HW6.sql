3292e249f2226d1cc7455105f5942b08d3b19ab8
Stainslav Gorozhankin <stas.gorozhankin@simplerfid.com>
2024-07-16 11:52:25 +0300

    hw6 added

---
+++ HW/HW6.sql
@@ -0,0 +1,272 @@
+﻿--Створіть наступні функції користувача (Functions): 
+
+--■ Функція користувача повертає вітання в стилі «Hello, ІМ'Я!» Де ІМ'Я передається як параметр. Наприклад, якщо передали Nick, то буде Hello, Nick! 
+create function Hello (@name varchar(50))
+	returns varchar(70)
+begin
+	return concat('Hello ', @name, '!');
+end
+
+go 
+
+select dbo.Hello('Nick')
+
+go
+--■ Функція користувача повертає інформацію про поточну кількість хвилин; 
+create function GetMinutes ()
+	returns tinyint
+begin
+	return datepart(minute, GETDATE());
+end
+
+go 
+
+select dbo.GetMinutes()
+
+go
+--■ Функція користувача повертає інформацію про поточний рік; 
+create function GetYear ()
+	returns smallint
+begin
+	return datepart(YEAR, GETDATE());
+end
+
+go 
+
+select dbo.GetYear()
+
+go
+
+--■ Функція користувача повертає інформацію про те: парний або непарний рік; 
+create function IsEvenYear ()
+	returns varchar(4)
+begin
+	declare @year smallint;
+	set @year = datepart(YEAR, GETDATE());
+	return iif(@year % 2 = 0, 'Even', 'Odd');
+end
+
+go 
+
+select dbo.IsEvenYear()
+
+go
+
+--■ Функція користувача приймає число і повертає yes, якщо число просте і no, якщо число не просте; 
+create function IsPrimeNumber (@number int)
+	returns varchar(3)
+begin
+	declare @isPrime bit
+	declare @counter int
+
+	set @isPrime = 1
+	set @counter = 2
+
+	while (@counter < @number )
+	begin
+		if (( @number % @counter) = 0 )
+		begin
+			set @isPrime = 0
+			break
+		end
+
+
+		set @counter = @counter + 1
+	end
+	return iif(@isPrime = 1, 'yes', 'no' )
+	
+end
+
+go 
+
+select dbo.IsPrimeNumber(108)
+
+go
+
+--■ Функція користувача приймає як параметри п'ять чисел. Повертає суму мінімального та максимального значення з переданих п'яти параметрів;
+
+create function MinMaxSum (@number1 int, @number2 int, @number3 int, @number4 int, @number5 int)
+	returns int
+begin
+	declare @numbers table(Id int);
+	insert into @numbers values (@number1), (@number2), (@number3), (@number4), (@number5)
+	
+	declare @result int
+
+	select @result = sum(Id) from
+			(select min(id) as Id from @numbers
+			union
+			select max(id) from @numbers) as tb
+
+	return @result
+end
+
+go 
+
+select dbo.MinMaxSum(5,56,45,99,3)
+
+go
+--■ Функція користувача показує всі парні або непарні числа в переданому діапазоні. Функція приймає три параметри: початок діапазону, кінець діапазону, парне чи непарне показувати.
+
+create function GetEvenNumbers (@start int, @end int)
+	returns  @evenNumbers table(Even int)
+as
+begin
+	while (@start <= @end )
+	begin
+		if(@start % 2 = 0)
+		begin
+			insert into @evenNumbers values (@start)
+		end
+
+		set @start += 1
+	end
+
+	return
+end
+
+go 
+
+select * from dbo.GetEvenNumbers(5,50)
+
+go
+
+--Створіть наступні збережені процедури (Stored procedures): 
+
+--■ Збережена процедура виводить «Hello, world!»; 
+create procedure HelloWorld as
+begin
+	select 'Hello, world!'
+end;
+go
+exec HelloWorld
+
+--■ Збережена процедура повертає інформацію про поточний час; 
+create procedure GetHour as
+begin
+	select datepart(HOUR, GETDATE());
+end;
+go
+exec GetHour
+
+--■ Збережена процедура повертає інформацію про поточну дату; 
+create procedure GetCurrentDate as
+begin
+	select GETDATE();
+end;
+go
+exec GetCurrentDate
+
+--■ Збережена процедура приймає три числа і повертає їхню суму; 
+
+create procedure  SumOfNumbers
+    @number1 int, @number2 int, @number3 int
+AS
+	declare @numbers table(Id int);
+	insert into @numbers values (@number1), (@number2), (@number3)
+	select sum(Id) as SumNumbers from @numbers
+
+go
+exec SumOfNumbers 5, 6, 7
+
+--■ Збережена процедура приймає три числа і повертає середньоарифметичне трьох чисел; 
+create procedure  AvgOfNumbers
+    @number1 int, @number2 int, @number3 int
+AS
+	declare @numbers table(Id int);
+	insert into @numbers values (@number1), (@number2), (@number3)
+	select avg(Id) as Average from @numbers
+
+go
+exec AvgOfNumbers 5, 6, 7
+
+--■ Збережена процедура приймає три числа і повертає максимальне значення; 
+create procedure  MaxOfNumbers
+    @number1 int, @number2 int, @number3 int
+AS
+	declare @numbers table(Id int);
+	insert into @numbers values (@number1), (@number2), (@number3)
+	select max(Id) as MaxNumber from @numbers
+
+go
+exec MaxOfNumbers 5, 7, 6
+
+--■ Збережена процедура приймає три числа і повертає мінімальне значення; 
+create procedure  MinOfNumbers
+    @number1 int, @number2 int, @number3 int
+AS
+	declare @numbers table(Id int);
+	insert into @numbers values (@number1), (@number2), (@number3)
+	select min(Id) as MinNumber from @numbers
+
+go
+exec MinOfNumbers 5, 7, 6
+
+
+--■ Збережена процедура приймає число та символ. 
+--В результаті роботи збереженої процедури відображається  лінія довжиною, що дорівнює числу. Лінія побудована із символу, вказаного у другому параметрі. 
+--Наприклад, якщо було передано 5 та #, ми отримаємо лінію такого виду #####; 
+create procedure DrawLine
+	@length INT, @character CHAR(1)
+as
+begin
+	declare @line nvarchar(MAX) = '';
+
+	while @length > 0
+	begin
+		set @line = @line + @character;
+		set @length = @length - 1;
+	end
+
+    print @line;
+end;
+
+go
+
+exec DrawLine 20, '*'
+
+--■ Збережена процедура приймає як параметр число і повертає його факторіал. 
+--Формула розрахунку факторіалу: n! = 1 * 2 * ... n. Наприклад, 3! = 1 * 2 * 3 = 6; 
+
+create procedure  Factorial
+	@number int
+AS
+begin
+	declare @factorial bigint;
+	set @factorial = 1;
+
+	while @number > 0
+	begin
+		set @factorial = @factorial * @number;
+		set @number = @number - 1;
+	end
+	select @factorial as Factorial
+end
+
+go
+
+exec Factorial 5
+
+--■ Збережена процедура приймає два числові параметри. Перший параметр – це число. 
+--Другий параметр – це ступінь. Процедура повертає число, зведене до ступеня. 
+--Наприклад, якщо параметри дорівнюють 2 і 3, тоді повернеться 2 у третьому ступені, тобто 8.
+
+create procedure Degree
+	@number int,
+	@degree int
+AS
+begin
+	declare @result bigint;
+	set @result = 1;
+
+	while @degree > 0
+	begin
+		set @result = @result * @number;
+		set @degree -= 1;
+	end
+	select @result as Degree;
+end
+
+go
+
+exec Degree 3, 5
---
+++ HW/HW6_additional.sql
@@ -0,0 +1,224 @@
+﻿--Використовуючи Triggers / Views або Updatable Views / Stored procedures / Functions реалізуйте наступну функціональність: 
+use Barbershop
+go
+--■ Повернути ПІБ всіх барберів салону. 
+create view BarbersFio as
+select FullName
+from Barbers
+go
+select * from BarbersFio
+go
+
+--■ Повернути інформацію про всіх синьйор-барберів. 
+create view SeniorBarber as
+select FullName, Gender, PhoneNumber, Email, DateOfBirth, HireDate
+from Barbers
+where Position like 'Senior%'
+go
+select * from SeniorBarber
+go
+
+--■ Повернути інформацію про всіх барберів, які можуть надати послугу традиційного гоління бороди. 
+create view BarberShave as
+select b.FullName, b.Gender, b.PhoneNumber, b.Email, b.DateOfBirth, b.HireDate, s.ServiceName
+from Barbers b
+join [Services] s
+on s.BarberID = b.BarberID
+where ServiceName = 'Shave'
+
+go
+select * from BarberShave
+go
+
+--■ Повернути інформацію про всіх барберів, які можуть надати конкретну послугу. Інформація про потрібну послугу надається як параметр.
+create procedure  GetBarbersByService
+	@service nvarchar(100)
+AS
+	select b.FullName, b.Gender, b.PhoneNumber, b.Email, b.DateOfBirth, b.HireDate, s.ServiceName
+		from Barbers b
+		join [Services] s
+		on s.BarberID = b.BarberID
+		where ServiceName = @service
+
+go
+exec GetBarbersByService 'Beard Trim'
+go
+
+--■ Повернути інформацію про всіх барберів, які працюють понад зазначену кількість років. Кількість років передається як параметр. 
+create procedure  GetBarbersByYear
+	@years int
+AS
+	select b.FullName, b.Gender, b.PhoneNumber, b.Email, b.DateOfBirth, b.HireDate
+		from Barbers b
+		where datediff(year, HireDate, GetDate()) > @years
+
+go
+exec GetBarbersByYear 2
+go
+
+--■ Повернути кількість синьйор-барберів та кількість джуніор-барберів. 
+create view GetBarberJuniorAndSenior as
+select Position, count(*) as Count
+from Barbers b
+where Position != 'Chief Barber'
+group by Position
+
+go
+select * from GetBarberJuniorAndSenior
+go
+
+--■ Повернути інформацію про постійних клієнтів. Критерій постійного клієнта: був у салоні задану кількість разів. Кількість передається як параметр. 
+
+create procedure  GetClientsByVisitCount
+	@visitCount int
+AS
+		select * 
+		from Clients 
+		where ClientID in (
+		select v.ClientID
+		from Visits v
+		group by v.ClientID
+		having count(*) > @visitCount)
+
+go
+exec GetClientsByVisitCount 7
+go
+
+--■ Заборонити можливість видалення інформації про чиф-барбер, якщо не додано другий чиф-барбер. 
+
+--■ Заборонити додавати барберів молодше 21 року
+alter table Barbers
+add constraint CK_Barber_Age check (DATEDIFF(YEAR, DateOfBirth, GETDATE()) >= 21);
+go
+
+insert into Barbers (FullName, Gender, PhoneNumber, Email, DateOfBirth, HireDate, Position)
+values
+('John Doe', 'Male', '1234567890', 'john@example.com', '2010-01-15', '2020-05-01', 'Chief Barber');
+go
+--create database Barbershop;
+--go
+
+--use Barbershop;
+
+--go
+
+--create table Barbers (
+--	BarberID int identity(1,1) primary key,
+--	FullName nvarchar(100) not null,
+--	Gender nvarchar(10) not null,
+--	PhoneNumber nvarchar(15) not null,
+--	Email nvarchar(100) not null,
+--	DateOfBirth date not null,
+--	HireDate date not null,
+--	Position nvarchar(50) not null check (Position IN ('Chief Barber', 'Senior Barber', 'Junior Barber'))
+--);
+
+--create table [Services] (
+--	ServiceID int identity(1,1) primary key,
+--	BarberID int NOT NULL,
+--	ServiceName nvarchar(100) not null,
+--	Price decimal(10, 2) not null,
+--	Duration time not null,
+--	foreign key (BarberID) REFERENCES Barbers(BarberID)
+--);
+
+--create table BarberFeedbacks (
+--	FeedbackID int identity(1,1) primary key,
+--	BarberID int not null,
+--	ClientID int not null,
+--	Feedback nvarchar(1000),
+--	Rating nvarchar(20) not null check (Rating IN ('Very Bad', 'Bad', 'Normal', 'Good', 'Excellent')),
+--	foreign key (BarberID) references Barbers(BarberID)
+--);
+
+--create table BarberSchedules (
+--	ScheduleID int identity(1,1) primary key,
+--	BarberID int not null,
+--	AvailableDate date not null,
+--	AvailableTime time not null,
+--	ClientID int,
+--	foreign key (BarberID) references Barbers(BarberID)
+--);
+
+--create table Clients (
+--	ClientID int identity(1,1) primary key,
+--	FullName nvarchar(100) not null,
+--	PhoneNumber nvarchar(15) not null,
+--	Email nvarchar(100) not null,
+--);
+
+--create table ClientFeedbacks (
+--	FeedbackID int identity(1,1) primary key,
+--	ClientID int not null,
+--	BarberID int not null,
+--	Feedback nvarchar(1000),
+--	Rating nvarchar(20) not null check (Rating IN ('Very Bad', 'Bad', 'Normal', 'Good', 'Excellent')),
+--	foreign key (ClientID) references Clients(ClientID),
+--	foreign key (BarberID) references Barbers(BarberID)
+--);
+
+--create table Visits (
+--	VisitID int identity(1,1) primary key,
+--	ClientID int not null,
+--	BarberID int not null,
+--	ServiceID int not null,
+--	VisitDate date not null,
+--	TotalCost decimal(10, 2) not null,
+--	Rating nvarchar(20) check (Rating in ('Very Bad', 'Bad', 'Normal', 'Good', 'Excellent')),
+--	Feedback nvarchar(1000),
+--	foreign key (ClientID) references Clients(ClientID),
+--	foreign key (BarberID) references Barbers(BarberID),
+--	foreign key (ServiceID) references [Services](ServiceID)
+--);
+
+--go
+
+--insert into Barbers (FullName, Gender, PhoneNumber, Email, DateOfBirth, HireDate, Position)
+--values
+--('John Doe', 'Male', '1234567890', 'john@example.com', '1985-01-15', '2020-05-01', 'Chief Barber'),
+--('Jane Smith', 'Female', '0987654321', 'jane@example.com', '1990-04-25', '2021-07-01', 'Senior Barber'),
+--('Mike Johnson', 'Male', '1112223333', 'mike@example.com', '1995-08-10', '2022-01-01', 'Junior Barber');
+
+--insert into [Services] (BarberID, ServiceName, Price, Duration)
+--values
+--(1, 'Haircut', 25.00, '00:30:00'),
+--(1, 'Shave', 15.00, '00:20:00'),
+--(2, 'Haircut', 20.00, '00:30:00'),
+--(2, 'Beard Trim', 10.00, '00:15:00'),
+--(3, 'Haircut', 15.00, '00:30:00');
+
+--insert into Clients (FullName, PhoneNumber, Email)
+--values
+--('Alice Brown', '2223334444', 'alice@example.com'),
+--('Bob White', '5556667777', 'bob@example.com');
+
+--insert into BarberSchedules (BarberID, AvailableDate, AvailableTime)
+--values
+--(1, '2023-07-20', '10:00:00'),
+--(1, '2023-07-20', '11:00:00'),
+--(2, '2023-07-21', '14:00:00'),
+--(3, '2023-07-22', '16:00:00');
+
+--insert into BarberFeedbacks (BarberID, ClientID, Feedback, Rating)
+--values
+--(1, 1, 'Great haircut!', 'Excellent'),
+--(2, 2, 'Good service.', 'Good');
+
+--insert into Visits (ClientID, BarberID, ServiceID, VisitDate, TotalCost, Rating, Feedback)
+--values
+--(1, 1, 1, '2023-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(2, 2, 3, '2023-07-11', 20.00, 'Good', 'Satisfied with the service.'),
+--(1, 1, 1, '2023-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(2, 2, 3, '2023-07-11', 20.00, 'Good', 'Satisfied with the service.'),
+--(1, 1, 1, '2023-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(2, 2, 3, '2023-07-11', 20.00, 'Good', 'Satisfied with the service.'),
+--(1, 1, 1, '2020-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(1, 2, 3, '2023-07-11', 20.00, 'Good', 'Satisfied with the service.'),
+--(1, 1, 1, '2023-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(2, 2, 3, '2024-07-11', 20.00, 'Good', 'Satisfied with the service.'),
+--(1, 1, 1, '2022-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(1, 2, 3, '2023-07-11', 20.00, 'Good', 'Satisfied with the service.'),
+--(1, 1, 1, '2021-07-10', 25.00, 'Excellent', 'Loved the haircut!'),
+--(2, 2, 3, '2023-07-11', 20.00, 'Good', 'Satisfied with the service.');
+
+
